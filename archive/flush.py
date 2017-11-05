a0261b20cfadcd1880bf889770351cea01e4e9d9
Dabson <c3093393@uon.edu.au>
2017-11-05 19:49:13 +1100

    'cleanup'

--- archive/flush.py
+++ archive/flush.py
@@ -1,44 +1,28 @@
-'''
-The following script resets ../index.html and moves it to /html
-'''
 import re
 from os import listdir
 from os.path import isfile, join
 import time
 import shutil
 
-log = open('log.txt', 'a') #open for writing - append mode
-filefrom = 'blankslate.html' #this is assumed in same dir as flush
-fileto   = '../index.html' # + filename[i]
+log = open('log.txt', 'a')   # open for writing - append mode
+filefrom = 'blankslate.html' # this is assumed in same dir as flush
+fileto   = '../index.html'
 archive  = 'html/'
-
 fileList = [f for f in listdir(archive) if isfile(join(archive, f))]
 
-max = 000
+maxno = 0
 for x in fileList:
-  if re.match('\d{3}.*', x):
-    if x[:3] > max:
-      max = int(x[:3])
-next = max + 1
+  try:
+    n = re.match('(\d+).*', x).group(1)
+  except: #aka catch
+    continue
+  if n > maxno:
+    maxno = int(n)
+nextno = maxno + 1
 
-shutil.copy2(fileto, archive+str(next)+'.html')
+shutil.copy2(fileto, archive+str(nextno).zfill(3)+'.html')
 shutil.copy2(filefrom, fileto)
-log.write('\n'+str(next).zfill(3) + '\t\t' + time.strftime("%I:%M %p %Y/%m/%d"))
-print 'Flushed index: ' + str(next)
-
-'''
-for i in range(0, fileList): #first 35 done manually. will start at 36 (index 35)
-	n = i+1
-	if(not os.path.isfile(fileto+filename[i])):
-		if not os.path.isdir(fileto):                # If the dir does not exist, create it :)
-			os.makedirs(fileto)
-
-		shutil.copy2(di, fileto+filename[i])  #copy out
-		shutil.copy2(filefrom, di) #copy in
-		log.write('\n'+str(n).zfill(3) + '\t\t' + time.strftime("%I:%M %p %Y/%m/%d"))
-		print 'Flushed index: ' + str(n) + '\n' + filename[i]
-		break
-'''
-
+log.write('\n'+str(nextno).zfill(3) + '\t\t' + time.strftime("%I:%M %p %Y/%m/%d"))
+print 'Flushed index: ' + str(nextno)
 log.close()
 
