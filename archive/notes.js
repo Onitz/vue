f49163ec7c5855f9685fbf30a08c76166be2d66e
Dabson <c3093393@uon.edu.au>
2017-11-06 18:32:13 +1100

    'singleLine'

--- archive/notes.js
+++ archive/notes.js
@@ -1,1 +1,22 @@
 //vuejs Start 6:11
+
+new Vue({ el:'', data{}, methods{} });
+  * Arguments are passed to directives(v-on) via a colon, ie vue-on:input="myMethod"
+        this.title = event.target.value;
+
+Course Structure:
+   1. Getting Started 
+   2. Interacting with the DOM (Templates)
+   3. Understanding the VueJS Instance 
+   4. Vue CLI 
+   5. Components 
+   6. Forms 
+   7. Directives, filteres & mixins 
+   8. Animations & transitions 
+   9. WORKING WITH HTTP 
+  10. Bigger/single page applications: Routing
+  11. State Management
+  12. Deployment
+  13. Four projects: basics, tempate interaction, components, animations, final project inc routing state managment 
+  (Source code attached to each video), exercises, Q&A
+
