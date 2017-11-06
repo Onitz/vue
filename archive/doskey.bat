6abe85af5b7db361c85c5b851635a56b79794614
Dabson <c3093393@uon.edu.au>
2017-11-06 19:44:23 +1100

    everything's coming up milhouse

--- archive/doskey.bat
+++ archive/doskey.bat
@@ -1,3 +1,4 @@
 REM vf used for flushing a file & commiting with first param as message
 REM Commit Message must be delimited with double quotes, $T splits commands
+REM Usage: vf "Commit message" -> flushes, adds, commits
 doskey vf=F: $T cd F:\Dropbox\projects\vue\archive $T python flush.py $T cd F:\Dropbox\projects\vue $T git add . $T git commit -m $1 $T git push
