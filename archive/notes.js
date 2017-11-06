df7d55588d3cb7a4178be3e225d6810a4dfb5a53
Dabson <c3093393@uon.edu.au>
2017-11-06 18:44:27 +1100

    'more'

--- archive/notes.js
+++ archive/notes.js
@@ -20,3 +20,20 @@ Course Structure:
   13. Four projects: basics, tempate interaction, components, animations, final project inc routing state managment 
   (Source code attached to each video), exercises, Q&A
 
+Vue instance + template(html)
+By instanciating this instance, you
+
+Vuejs does not use htmlcode at runtime. 
+  It creates a template based on htmlcode, 
+  stores internally, 
+  then renders real html code to the DOM
+There is this VueJs layer instance in the middle
+{{ /*you can execute functions and eval expressions here */ }}
+<p>{{ sayHello() }}</p>
+
+^in the template, we have access to everything stored in:
+  data
+  methods
+  (because vuejs proxies them for us)
+  we can call this. anywhere in our Vue Instance object
+  to get access to any of our vue properties
