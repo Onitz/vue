c838c3b96b927871902599d42b4b4bfe130b9eff
Dabson <c3093393@uon.edu.au>
2017-11-06 19:40:55 +1100

    hello

---
+++ archive/doskey.bat
@@ -0,0 +1,3 @@
+REM vf used for flushing a file & commiting with first param as message
+REM Commit Message must be delimited with double quotes, $T splits commands
+doskey vf=F: $T cd F:\Dropbox\projects\vue\archive $T python flush.py $T cd F:\Dropbox\projects\vue $T git add . $T git commit -m $1 $T git push
