060aed5cf9940c6f670ce6d70d72fba2fe741e78
Dabson <c3093393@uon.edu.au>
2017-11-19 12:23:40 +1100

    a

--- archive/doskey.bat
+++ archive/doskey.bat
@@ -2,6 +2,5 @@ REM Usage: vf "Commit message" -> flushes, adds, commits with first param as mes
 REM regedit HKEY_CURRENT_USER/Software/Microsoft/Command Processor/AutoRun(string) [dir]\doskey.bat
 REM SET gdir = F:\git
 REM SET gidr = F:\Dropbox\projects
-REM doskey vf=F: $T cd %gdir%\vue\archive $T python flush.py $T cd %gdir%\vue $T git add . $T git commit -m "$1" $T git push
-REM the above broke multiline
-doskey vf=F: $T cd F:\Dropbox\projects\vue\archive $T python flush.py $T cd F:\Dropbox\projects\vue $T git add . $T git commit -m $1 $T git push
+REM doskey vf=F: $T cd %gdir%\vue\archive $T python flush.py $T cd %gdir%\vue $T git add . $T git commit -m "$*" $T git push
+doskey vf=F: $T cd F:\Dropbox\projects\vue\archive $T python flush.py $T cd F:\Dropbox\projects\vue $T git add . $T git commit -m "$*" $T git push
