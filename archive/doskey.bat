84d29bfe83c367cf5bedfd5f2779880378a29424
Dabson <c3093393@uon.edu.au>
2017-11-25 14:25:21 +1100

    q b c

--- archive/doskey.bat
+++ archive/doskey.bat
@@ -1,4 +1,5 @@
-REM Usage: vf "Commit message" -> flushes, adds, commits with first param as message (delimited with double quotes)
+REM Usage: vf commit message -> flushes, adds, commits with first param as message (not delimited with any qutoes)
 REM regedit HKEY_CURRENT_USER/Software/Microsoft/Command Processor/AutoRun(string) [dir]\doskey.bat
 REM SET gdir=F:\git
-doskey vf=F: $T cd F:\Dropbox\projects\vue\archive $T python flush.py $T cd F:\Dropbox\projects\vue $T git add . $T git commit -m "$*" $T git push
+SET gdir=F:\Dropbox\projects
+doskey vf=F: $T cd %gdir%\vue\archive $T python flush.py $T cd %gdir%\vue $T git add . $T git commit -m "$*" $T git push
