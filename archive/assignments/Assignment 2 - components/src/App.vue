5d2961143f4032d0ae22941c256a1e686552d20f
Dabson <c3093393@uon.edu.au>
2018-01-01 15:56:06 +1100

    cleanup

--- archive/assignments/Assignment 2 - components/src/App.vue
+++ archive/assignments/Assignment 2 - components/src/App.vue
@@ -9,11 +9,8 @@
 </template>
 
 <script>
-    import dabServerStatus from './dabServerStatus.vue'
-    import dabServerDetails from './dabServerDetails.vue'
     import dabHeader from './dabHeader.vue'
     import dabFooter from './dabFooter.vue'
-    import dabServer from './dabServer.vue'
     import dabBody from './dabBody.vue'
 
     export default {
