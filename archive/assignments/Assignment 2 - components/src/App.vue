e2738adb72dad6cb8e6e0f541b533b140e4055a8
Dabson <c3093393@uon.edu.au>
2018-01-01 15:55:05 +1100

    Assignment 2 - splitting out components

---
+++ archive/assignments/Assignment 2 - components/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <dabHeader></dabHeader>
+        <hr>
+        <dabBody></dabBody>
+        <hr>
+        <dabFooter></dabFooter>
+    </div>
+</template>
+
+<script>
+    import dabServerStatus from './dabServerStatus.vue'
+    import dabServerDetails from './dabServerDetails.vue'
+    import dabHeader from './dabHeader.vue'
+    import dabFooter from './dabFooter.vue'
+    import dabServer from './dabServer.vue'
+    import dabBody from './dabBody.vue'
+
+    export default {
+        components: {
+            dabHeader,
+            dabFooter,
+            dabBody
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ archive/assignments/Assignment 2 - components/src/dabBody.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+      <dabServerStatus></dabServerStatus>
+      <dabServerDetails></dabServerDetails>
+  </div>
+</template>
+
+<script>
+    import dabServerStatus from './dabServerStatus.vue'
+    import dabServerDetails from './dabServerDetails.vue'
+
+    export default {
+        components: {
+            dabServerStatus,
+            dabServerDetails
+        }
+    }
+</script>
