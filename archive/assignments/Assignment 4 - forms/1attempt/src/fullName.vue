4d01d5c01c85f8fddae3b65e9af88f32fcfc9500
Dabson <cyphlix@gmail.com>
2018-01-27 00:03:13 +1100

    ass4 forms

---
+++ archive/assignments/Assignment 4 - forms/1attempt/src/fullName.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    i am full name
+    <input :value="fullName" @input="updateName" ref="fn">
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['fullName'],
+  methods: {
+    updateName(v) {
+      console.log(v);
+      console.log(this.$refs.fn)
+      this.$emit('input', this.$refs.fn.value);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
